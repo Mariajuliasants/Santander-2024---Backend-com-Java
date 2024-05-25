536ca62bf42b77ba8ac70ebef5e644b233c8d7ca
Mariajuliasants <contmariajulia@gmail.com>
2024-05-25 16:30:17 -0300

    Metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       
+    SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual:"+smartTv.volume);
+
+
+    System.out.println("Tv ligada? "+smartTv.ligada);
+  
+    System.out.println("Volume atual:"+smartTv.volume);
+
+    System.out.println("Canal atual: "+smartTv.canal);
+    smartTv.mudarCanal(13);
+    System.out.println("Canal atual: "+smartTv.canal);
+
+    smartTv.ligar();
+    System.out.println(" Novo status  --> Tv ligada? "+smartTv.ligada);
+
+    }
+
+}
